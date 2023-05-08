49658f1041f2ebb52c72a9998490ea0f2a5001b6
JasperLefeverHogent <99969350+JasperLefeverHogent@users.noreply.github.com>
2023-05-08 11:11:27 +0200

    added @Transactional

--- Spring_Boot_JPA_DocentCampus_start/src/main/java/service/SchoolService.java
+++ Spring_Boot_JPA_DocentCampus_start/src/main/java/service/SchoolService.java
@@ -1,6 +1,9 @@
 package service;
 
+import jakarta.transaction.Transactional;
+
 public interface SchoolService {
 
+	@Transactional
 	public void changeWerkruimte(String lokaalCode, String campusNaam1, String campusNaam2);
 }
